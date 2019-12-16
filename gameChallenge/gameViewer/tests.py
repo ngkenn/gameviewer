4c5b1bf37e2c7d33c554a5aea4a6a3c15f3e5655
Neil Kennedy <2025066K@student.gla.ac.uk>
2019-12-16 16:24:54 +1100

    mad test

--- gameChallenge/gameViewer/tests.py
+++ gameChallenge/gameViewer/tests.py
@@ -1,3 +1,17 @@
+from gameViewer.models import Game
 from django.test import TestCase
 
-# Create your tests here.
+# Very basic test to check if object saved and slugified properly
+class GameModelTest(TestCase):
+    def setUp(self):
+        Game.objects.create(name="GOD OF WAR", platform="PS4",
+         genre="FIGHTING", release_date="2018-01-07", 
+         n_players=4, publisher="SONY INTERACTIVE ENTERTAINMENT")
+
+    def test_game_slug(self):
+        god_of_war = Game.objects.get(name="GOD OF WAR")
+        god_of_war.save()
+        self.assertEqual(god_of_war.slug, "god-of-war")
+
+
+
